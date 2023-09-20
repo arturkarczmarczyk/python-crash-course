ad7b5c5b4ed45ac7228c76c9281cda0a4129fd0a
Artur Karczmarczyk <artur@ideaspot.pl>
2023-09-20 14:18:36 +0200

    chapter5 if statements

---
+++ chapter5if/alien2.py
@@ -0,0 +1,6 @@
+alien_color = 'red'
+
+if (alien_color == 'green'):
+    print("You scored 5 points!")
+else:
+    print("You scored 10 points!")
---
+++ chapter5if/in.py
@@ -0,0 +1,7 @@
+ingredient = 'veggie'
+ingredients = ['salt', 'pepper', 'onion']
+
+if ingredient in ingredients:
+    print(f"{ingredient} is in {ingredients}")
+else:
+    print(f"{ingredient} is not in {ingredients}")
