4dac5ee2bb1ed0fe9a2b62cb3789e08cd3715936
Artur Karczmarczyk <artur@ideaspot.pl>
2023-09-20 17:00:20 +0200

    chapter15 data visualization

---
+++ chapter15generatingdata/rw_visual.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+from random_walk import RandomWalk
+
+# Make a random walk
+rw = RandomWalk(50_000)
+rw.fill_walk()
+
+# Plot the points in the walk
+plt.style.use('classic')
+fig, ax = plt.subplots(figsize=(16,9), dpi=128)
+point_numbers = range(rw.num_points)
+ax.scatter(rw.x_values, rw.y_values, s=1,
+           c=point_numbers, cmap=plt.cm.Blues, edgecolors='none')
+ax.scatter(0, 0, c='green', edgecolors='none', s=100)
+ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+# remove the axes
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+
+plt.show()
