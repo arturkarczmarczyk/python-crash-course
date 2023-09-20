95969c34ef966e3ad8160cb334d04a9cfb17c71a
Artur Karczmarczyk <artur@ideaspot.pl>
2023-09-20 14:11:54 +0200

    chapter4 working with lists and tuples

---
+++ chapter4/pizzas.py
@@ -0,0 +1,6 @@
+pizzas = ['hawaii', 'mafia', 'capriciosa']
+
+for pizza in pizzas:
+    print(f"I like {pizza} pizza!")
+
+print("I like all pizzas very much!")
---
+++ chapter4/players.py
@@ -0,0 +1,6 @@
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[0:3])
+print(players[1:4])
+
+last_three = players[-3:]
+print(last_three)
